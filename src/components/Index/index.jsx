3220fa8a37fed21b1525cfcfc15ae1bb09854315
D-nDev <diego-s.novaes@hotmail.com>
2021-06-12 17:44:53 -0300

    Initialize project using Create React App

---
+++ src/components/Index/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Index() {
+  return <div />;
+}
+
+export default Index;
---
+++ src/providers/indexProvider.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function providers() {
+  return <div />;
+}
+
+export default providers;
